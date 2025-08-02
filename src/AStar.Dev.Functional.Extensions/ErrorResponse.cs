1ad11cb79f41e980e43070c5401b13b71a352dbf
Jason Barden <jason.barden@outlook.com>
2025-08-02 01:31:43 +0100

    Add the utilities and associated tests from the main solution

---
+++ src/AStar.Dev.Functional.Extensions/ErrorResponse.cs
@@ -0,0 +1,13 @@
+namespace AStar.Dev.Functional.Extensions;
+
+/// <summary>
+///
+/// </summary>
+/// <param name="Message"></param>
+public class ErrorResponse
+{
+    /// <summary>
+    /// Represents the message associated with an error response.
+    /// </summary>
+    public string Message { get; set; } = string.Empty;
+}
